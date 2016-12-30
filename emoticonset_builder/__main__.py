57b7f4af948a62e1da1d8cd7a6b8bd3ca6ec9b47
Nicholas <krylik@rigaming.co.za>
2016-12-30 20:35:45 +0200

    Fixed filename case sensitivity issues

--- emoticonset_builder/__main__.py
+++ emoticonset_builder/__main__.py
@@ -1,6 +1,7 @@
 __author__ = 'Krylik'
 import argparse
 import re
+import os
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from shutil import copytree, ignore_patterns
@@ -16,7 +17,10 @@ argparser.add_argument('outdir',
                        help="The output directory name, this will have .AdiumEmoticonSet appended to the name")
 args = argparser.parse_args()
 
-copytree(args.indir, args.outdir, ignore=ignore_patterns('theme', '.git*', '*.gz'))
+outdir = "%s%s" % (args.outdir, '.AdiumEmoticonSet')
+copytree(args.indir, outdir, ignore=ignore_patterns('theme', '.git*', '*.gz'))
+# store list of filenames for case insensitive matching later
+outfiles = os.listdir(outdir)
 
 emoticons = []
 with open('%s/theme' % args.indir) as themefile:
@@ -34,6 +38,10 @@ plist_root_dict = plist.find('dict')
 emote_dict = ET.SubElement(plist_root_dict, 'dict')
 for emote in emoticons:
     img, triggers = emote
+    # verify image name, replace with correct case if necessary
+    for filename in outfiles:
+        if filename.lower() == img.lower():
+            img = filename
     # key for image name
     img_key = ET.SubElement(emote_dict, 'key')
     img_key.text = img
@@ -52,5 +60,5 @@ for emote in emoticons:
 
 # Write xml output to the outfile specified
 reparsed_xml = minidom.parseString(ET.tostring(plist))
-with open('%s/Emoticons.plist' % args.outdir, 'w+') as outplist:
+with open('%s/Emoticons.plist' % outdir, 'w+') as outplist:
     outplist.write(reparsed_xml.toprettyxml(indent="\t"))
